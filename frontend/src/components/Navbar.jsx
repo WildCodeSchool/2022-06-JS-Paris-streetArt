dda3f9cc1997505cb9a1fbe05acc4ca377aa0981
Cedric.C <kmuced@gmail.com>
2022-07-18 09:59:49 +0200

    modif du mecredi 13 juillet

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,42 +11,48 @@ function Nav() {
   };
 
   return (
-    <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
-      <div className="navbar_logo" logo>
-        <img src={Logo} alt="Logo" height={50} />
-      </div>
-      <ul className="navbar_links">
-        <li className="navbar_items slideInDown-1">
-          <NavLink to="/artistes" className="navbar_link">
-            Artistes
-          </NavLink>
-        </li>
-        <li className="navbar_items slideInDown-2">
-          <NavLink to="/gallerie" className="navbar_link">
-            Gallerie
-          </NavLink>
-        </li>
-        <li className="navbar_items slideInDown-3">
-          <NavLink to="/home" className="navbar_link">
-            <img id="logo" src={Logo} alt="Logo" height={50} />
-            <span id="home">Home</span>
-          </NavLink>
-        </li>
-        <li className="navbar_items slideInDown-4">
-          <NavLink to="/localisation" className="navbar_link">
-            Localisation
-          </NavLink>
-        </li>
-        <li className="navbar_items slideInDown-5">
-          <NavLink to="/contact" className="navbar_link">
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <button type="button" className="navbar_burger" onClick={handleShowLinks}>
-        <span className="burger_bar" />
-      </button>
-    </nav>
+    <header>
+      <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
+        <div className="navbar_logo" logo>
+          <img src={Logo} alt="Logo" height={50} />
+        </div>
+        <ul className="navbar_links">
+          <li className="navbar_items slideInDown-1">
+            <NavLink to="/artistes" className="navbar_link">
+              Artistes
+            </NavLink>
+          </li>
+          <li className="navbar_items slideInDown-2">
+            <NavLink to="/gallerie" className="navbar_link">
+              Gallerie
+            </NavLink>
+          </li>
+          <li className="navbar_items slideInDown-3">
+            <NavLink to="/home" className="navbar_link">
+              <img id="logo" src={Logo} alt="Logo" height={50} />
+              <span id="home">Home</span>
+            </NavLink>
+          </li>
+          <li className="navbar_items slideInDown-4">
+            <NavLink to="/localisation" className="navbar_link">
+              Localisation
+            </NavLink>
+          </li>
+          <li className="navbar_items slideInDown-5">
+            <NavLink to="/contact" className="navbar_link">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="navbar_burger"
+          onClick={handleShowLinks}
+        >
+          <span className="burger_bar" />
+        </button>
+      </nav>
+    </header>
   );
 }
 
