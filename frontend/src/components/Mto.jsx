acb63802c49b08d5a570d348a75c1cf07d2d8a10
AndreaCastel <marion.chateau@sncf.fr>
2022-07-21 08:04:25 +0200

    CSS Last 07.21 AM

--- frontend/src/components/Mto.jsx
+++ frontend/src/components/Mto.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Mto() {
   return (
-    <div className="banksy-img">
+    <div className="Mto-img">
       <h2>Oeuvres de MTO</h2>
 
       <img
@@ -10,42 +10,42 @@ function Mto() {
         src="http://img.over-blog.com/600x450/5/40/75/79/D.png"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
       <img
         className="Mto"
         src="https://street-art-avenue.com/wp-content/uploads/2013/07/MTO-jack-nicholson-heinrichplatz-kreuzberg-berlin-4.jpg"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
       <img
         className="Mto"
         src="https://lowlifemagazine.files.wordpress.com/2016/11/mto-1.jpg?w=980"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
       <img
         className="Mto"
         src="https://i.pinimg.com/originals/44/bb/7b/44bb7ba1dd4512c2d335e44066e56848.jpg"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
       <img
         className="Mto"
         src="https://i.pinimg.com/736x/40/6c/01/406c017437848f7ed9d8299d467c34df--malta-art-festival.jpg"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
       <img
         className="Mto"
         src="https://isupportstreetart.blob.core.windows.net/issabucket/Stuff/street_art/MTO/MTO_issa_30.jpg"
         alt=""
         height={200}
-        width={250}
+        width={210}
       />
     </div>
   );
