d572554dc312164c00e4d7e8598252d3ffb1be16
AndreaCastel <marion.chateau@sncf.fr>
2022-07-21 09:56:23 +0200

    CSS clean

--- frontend/src/components/DescriptionC215.jsx
+++ frontend/src/components/DescriptionC215.jsx
@@ -13,38 +13,6 @@ function DescriptionC215() {
         toutefois avec du matériel que possédait sa mère et dans sa jeunesse, il
         réalise des bandes dessinées pour le journal de l'école ainsi que des
         caricatures de professeurs et d'élèves.
-        {/* « Adolescent, j’avais un peu
-        taggé, mais ça n’avait rien de sérieux », confie-t-il. À l'été 1989, à
-        l'âge de 15 ans, il graphe un peu, mais laisse tomber, ne se sentant pas
-        en adéquation avec le mouvement hip-hop. Christian Guémy s'installe à
-        Vitry-sur-Seine en 2006 et commence à faire du pochoir. Il participe au
-        MUR dès 2007 et réalise, en 2013, un mur peint de 25 mètres à Paris,
-        métro Nationale, représentant un chat.
-        {/* En 2013, il peint également le
-        visage de la ministre de la Justice, Christiane Taubira, alors cible
-        d'attaques racistes. Le 4 janvier 2016, l'artiste réalise au pochoir sur
-        une boîte à feu un portrait multiple du policier Ahmed Merabet orné du
-        fameux « Je suis Ahmed ». L'œuvre a été réalisée à la demande du
-        commandant Stéphane Motel, du commissariat du 11e arrondissement de
-        Paris où travaillait Ahmed. Le portrait, conçu sur le boulevard
-        Richard-Lenoir, a été dévoilé devant une centaine de personnes parmi
-        lesquelles les collègues du policier et sa famille au complet. À
-        l'occasion de la panthéonisation de la magistrate et femme d'État
-        française, Simone Veil, C215 avait réalisé deux portraits de cette femme
-        déportée à Auschwitz-Birkenau parce que juive sur les boîtes aux lettres
-        de la mairie du 13e arrondissement, à Paris. Début juin 2020 il réalise
-        le portrait d'Aïcha Issadounène, première caissière victime du Covid,
-        sur un mur de la ville de Saint-Ouen où elle résidait. Fin juillet, il
-        réalise le portrait du docteur Jean-Jacques Razafindranazy, l’un des
-        premiers médecins décédé du Covid-19 en France, à l’entrée du centre
-        hospitalier de Soissons où il exerçait, en présence du Maire de la ville
-        et de la famille de l'urgentiste décédé. C215 intervient dans les rues
-        du monde entier depuis le début des années 2000. Il présente ses œuvres
-        peintes sur des objets ou sur toiles dans plusieurs galeries, en France
-        et à l’étranger. Il a exposé dans de nombreux musées nationaux et
-        collabore régulièrement avec des institutions publiques, culturelles ou
-        sociales. C215 est considéré aujourd’hui comme l’un des pochoiristes les
-        plus reconnus de la scène Street art mondiale. */}
       </p>
     </div>
   );
