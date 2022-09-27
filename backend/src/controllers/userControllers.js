b82593ba1090ad9af5ca26dedd3c94acc1092c08
AndreaCastel <marion.chateau@sncf.fr>
2022-09-27 18:41:05 +0200

    userControllers

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,12 @@
+// const signup = (req, res) => {
+
+// res.status(200).json({
+//     msg: "user created with success",
+//     token:
+//     // user: { name, email }
+//     });
+// };
+
+// module.exports = {
+//     signup,
+// }
